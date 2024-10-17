ea753169d2bfc3a481d8b27521a2c5b2e46c0d74
Ezequiel Maidana <ezemaidana1889@gmail.com>
2024-10-17 12:17:43 -0300

    comenzando

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,32 @@
+
+
+const NavBar = () =>{
+    return(
+        <header>
+            <nav className="nav-cont">
+                <div className="img-cont">
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    <h1>Titulo</h1>
+                </div>
+
+                <div>
+                    <ul>
+                        <li>Tienda</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+    )
+}
+
+
+
+
+export default NavBar;
