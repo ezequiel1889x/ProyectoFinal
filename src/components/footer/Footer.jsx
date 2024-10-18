f63276a9b0ac682fc8d5a00ecb143ae13d539585
Ezequiel Maidana <ezemaidana1889@gmail.com>
2024-10-18 12:51:10 -0300

    comenzando

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,6 @@
-
 const Footer = () => {
     return(
-        <footer>
+        <footer style={{height:'30%', backgroundColor:'#135',color:'#567',display:'flex',width:'100%',justifyContent:'space-around',alignItems:'center'}}>
             <div className="copyFooter">
                 <h3>Copy</h3>
             </div>
@@ -11,7 +10,7 @@ const Footer = () => {
             </div>
 
             <div className="redes">
-                <ul>
+                <ul style={{listStyle:'none'}}>
                     <li>Whatsapp</li>
                     <li>Instagram</li>
                     <li>Gmail</li>
