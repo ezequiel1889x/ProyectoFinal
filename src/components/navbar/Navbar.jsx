e6e27c71c4cab2c38e5f1acff665c3bd75188858
Ezequiel Maidana <ezemaidana1889@gmail.com>
2024-10-24 20:01:54 -0300

    camb2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,28 @@
 
 const navbar = () => {
     return(
-        <header>
-            <nav style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-                <div className="imgCont">
-                    <img src="" alt="ImagenNav" />
-                </div>
-
-                <div className="titulo">
-                    <h1>Titulo</h1>
-                </div>
-
-                <div className="ulCont">
-                    <ul style>
-                        <li>Tienda</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <>
+            <header style={{height:'17%',backgroundColor:'#135',color:'#567', borderRadius:'5px'}}>
+                <nav style={{display:'flex',justifyContent:'space-around',alignItems:'center', height:'100%'}}>
+                    <div className="imgCont">
+                        <img src="" alt="ImagenNav" />
+                    </div>
+        
+                    <div className="titulo">
+                        <h1>Titulo</h1>
+                    </div>
+        
+                    <div className="ulCont">
+                        <ul style={{listStyle:'none',display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center', gap:'8px'}}>
+                            <li style={{color:'white', textDecoration:'none'}}>Tienda</li>
+                            <li style={{color:'white', textDecoration:'none'}}>Ofertas</li>
+                            <li style={{color:'white', textDecoration:'none'}}>Contacto</li>
+                            <li style={{color:'white', textDecoration:'none'}}>Nosotros</li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
     )
 }
 
